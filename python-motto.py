c48e976787ffc54a1af0207f8f690c8a1e7c5965
duyanbin <yanghigh@163.com>
2021-01-10 20:58:40 +0800

    change the format

--- python-motto.py
+++ python-motto.py
@@ -1,58 +1,57 @@
 @Karma
 def live_a_lifetime():
+	"""
+	万般将不去，唯有业随身。 -- 《龙舒增广净土文》
+	Karma: 业力
+	Buddha: 佛陀(觉悟的人)
+	Samsara: 轮回
+	"""
 	while not Buddha():
 		Samsara()
 	return Buddha
 
-"""
-万般带不走，唯有业随身。
-"""
-
-
 def who_dreamed_a_dream(Zhuangzhou, Butterfly):
+	"""
+	不知周之梦为胡蝶与，胡蝶之梦为周与？周与胡蝶，则必有分矣。此之谓物化。 -- 《庄子·齐物论》
+	"""
 	Zhuangzhou.magically_change_to = Butterfly
 	Butterfly.magically_change_to = Zhuangzhou
 	return Zhuangzhou != Butterfly
 
-"""
-不知周之梦为胡蝶与，胡蝶之梦为周与？周与胡蝶，则必有分矣。此之谓物化。 -- 《庄子·齐物论》
-"""
-
-
 def tao():
+	"""
+	道生一，一生二，二生三，三生万物。  -- 《道德经》
+	"""
 	yield 1
 	yield 2
 	yield 3
 	yield all
-"""
-道生一，一生二，二生三，三生万物。  -- 《道德经》
-"""
 
 
 class Time:
+	"""
+	逝者如斯夫，不舍昼夜。 -- 《论语》
+	"""
 	def flies(self):
 		return River.flow(day, night)
-"""
-逝者如斯夫，不舍昼夜。 -- 《论语》
-"""
 
 
 class RealMan:
+	"""
+	富贵不能淫，贫贱不能移，威武不能屈，此之谓大丈夫。  -- 《孟子·滕文公下》
+	"""
 	def be_wealthy(self):
-		return honest
+		return be_honest
 
 	def be_poor(self):
-		return positive
+		return be_positive
 
 	def be_coerced(self):
-		return resistance
-"""
-富贵不能淫，贫贱不能移，威武不能屈，此之谓大丈夫。  -- 《孟子·滕文公下》
-"""
+		return be_resistance
 
 
 def is_worth_living(Life):
+	"""
+	未经审视的人生不值得度过。 -- 苏格拉底
+	"""
 	return Life.be_examined
-"""
-未经审视的人生不值得度过。 -- 苏格拉底
-"""
