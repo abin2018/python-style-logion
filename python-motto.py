103c8d73d6896e1655e81d909a4d65cec72fe408
duyanbin <yanghigh@163.com>
2021-01-10 20:47:35 +0800

    create python-motto file

---
+++ python-motto.py
@@ -0,0 +1,58 @@
+@Karma
+def live_a_lifetime():
+	while not Buddha():
+		Samsara()
+	return Buddha
+
+"""
+万般带不走，唯有业随身。
+"""
+
+
+def who_dreamed_a_dream(Zhuangzhou, Butterfly):
+	Zhuangzhou.magically_change_to = Butterfly
+	Butterfly.magically_change_to = Zhuangzhou
+	return Zhuangzhou != Butterfly
+
+"""
+不知周之梦为胡蝶与，胡蝶之梦为周与？周与胡蝶，则必有分矣。此之谓物化。 -- 《庄子·齐物论》
+"""
+
+
+def tao():
+	yield 1
+	yield 2
+	yield 3
+	yield all
+"""
+道生一，一生二，二生三，三生万物。  -- 《道德经》
+"""
+
+
+class Time:
+	def flies(self):
+		return River.flow(day, night)
+"""
+逝者如斯夫，不舍昼夜。 -- 《论语》
+"""
+
+
+class RealMan:
+	def be_wealthy(self):
+		return honest
+
+	def be_poor(self):
+		return positive
+
+	def be_coerced(self):
+		return resistance
+"""
+富贵不能淫，贫贱不能移，威武不能屈，此之谓大丈夫。  -- 《孟子·滕文公下》
+"""
+
+
+def is_worth_living(Life):
+	return Life.be_examined
+"""
+未经审视的人生不值得度过。 -- 苏格拉底
+"""
